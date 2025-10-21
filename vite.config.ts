2e32f917c0a33be9b01b81b4ffe72a79a18cf78b
server9o1 <98536087+server9o1@users.noreply.github.com>
2025-10-22 02:49:48 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({base: '/My-website
+/',}) => ({
   server: {
     host: "::",
     port: 8080,
